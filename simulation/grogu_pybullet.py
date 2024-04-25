f40250c2bbb81b84825145df1b5add43406ae5e8
Shreeya Patel <shreeyapaatel@gmail.com>
2024-04-25 19:51:54 -0400

    /Users/shreeyapatel/.pyenv/versions/3.9.18/bin/python

---
+++ simulation/grogu_pybullet.py
@@ -0,0 +1,25 @@
+import pybullet as p
+import time
+import pybullet_data
+
+pysicsClient = p.connect(p.GUI) #p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-9.81)
+
+groundId = p.loadURDF("plane.urdf")
+robotStartPos = [0,0,2]
+robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
+robotId = p.loadURDF("grogu.urdf", robotStartPos, robotStartOrientation)
+
+urdf = ""
+
+for i in range(10000): 
+    p.stepSimulation()
+    # time.sleep(1./240.)
+    time.sleep(1./1000.)
+    robotPos, robotOrn = p.getBasePositionAndOrientation(robotId)
+    print(robotPos, robotOrn)
+
+p.disconnect()
+
+
